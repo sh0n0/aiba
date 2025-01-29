3930f87736da3b81cece3fafaef7cbbe5a06c6af
sh0n0 <44708388+sh0n0@users.noreply.github.com>
2025-01-29 17:58:49 +0900

    Add letter_opener_web

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "letter_opener" if Rails.env.development?
+
   mount_devise_token_auth_for "User", at: "auth"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
