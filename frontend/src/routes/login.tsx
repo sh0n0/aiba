370f19498a1ea1bf2b49fbb14d751e989e9044aa
sh0n0 <44708388+sh0n0@users.noreply.github.com>
2025-02-05 18:43:31 +0900

    Use React Hook Form

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,10 +1,10 @@
 import { loginFetcher } from "@/api/login";
 import { Button } from "@/components/ui/button.tsx";
+import { Form, FormField, FormLabel } from "@/components/ui/form.tsx";
 import { Input } from "@/components/ui/input.tsx";
-import { Label } from "@/components/ui/label.tsx";
 import { useAppStore } from "@/store/store.ts";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import useSWRMutation from "swr/mutation";
 
 export const Route = createFileRoute("/login")({
@@ -14,13 +14,12 @@ export const Route = createFileRoute("/login")({
 function Login() {
   const setAuth = useAppStore((state) => state.setAuth);
   const navigate = useNavigate({ from: "/login" });
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const form = useForm({ defaultValues: { email: "", password: "" } });
 
   const { trigger, isMutating, error } = useSWRMutation("sign_in", loginFetcher);
 
-  const onLoginClicked = async () => {
+  const onLoginClicked = async (data: { email: string; password: string }) => {
+    const { email, password } = data;
     const { uid, client, accessToken } = await trigger({ email, password });
     setAuth(uid, client, accessToken);
     await navigate({ to: "/" });
@@ -28,31 +27,30 @@ function Login() {
 
   return (
     <div className="-mt-12 flex h-screen flex-col items-center justify-center">
-      <div className="grid gap-8">
-        <div>
-          <Label htmlFor="email">Email</Label>
-          <Input
-            className="w-80"
-            placeholder="aiba@example.com"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onLoginClicked)}>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field: { onChange } }) => <Input type={"email"} autoComplete={"email"} onChange={onChange} />}
           />
-        </div>
-        <div>
-          <Label htmlFor="password">Password</Label>
-          <Input
-            className="w-80"
-            placeholder="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field: { onChange } }) => (
+              <Input type={"password"} autoComplete={"current-password"} onChange={onChange} />
+            )}
           />
-        </div>
-      </div>
-      {error && <div className="mt-4 text-red-500">{error.message}</div>}
-      <Button className="mt-12 w-80" onClick={onLoginClicked} disabled={isMutating}>
-        {isMutating ? "Logging in..." : "Login"}
-      </Button>
+
+          <Button type="submit" disabled={isMutating}>
+            {isMutating ? "Logging in..." : "Login"}
+          </Button>
+          {error && <div className="mt-2 text-red-500">Error: {error.message}</div>}
+        </form>
+      </Form>
     </div>
   );
 }
