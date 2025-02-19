325b1ac77e400b64800eb7f07beb9d4a95768558
sh0n0 <44708388+sh0n0@users.noreply.github.com>
2025-02-19 14:17:16 +0900

    Rename

--- backend/spec/requests/companions/show_spec.rb
+++ backend/spec/requests/companions/show_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe CompanionsController, type: :request do
   describe 'GET /companions/:account_name/:companion_name' do
     let!(:account) { create(:account) }
     let!(:user) { create(:user, account: account) }
-    let!(:companion) { create(:companion, creator: account) }
+    let!(:companion) { create(:companion, creator: account, published_at: Time.current) }
     let!(:companion_ownership) { create(:companion_ownership, account: account, companion: companion) }
 
     context 'when the companion exists' do
@@ -36,6 +36,25 @@ RSpec.describe CompanionsController, type: :request do
       it 'returns a 404' do
         get "/companions/#{account.name}/#{other_companion.name}", headers: auth_headers(user)
         expect(response).to have_http_status(404)
+      end
+    end
+
+    context 'when the companion is not published' do
+      let!(:companion) { create(:companion, creator: account, published_at: nil) }
+      context 'when the requesting user is the account owner' do
+        it 'returns the companion' do
+          get "/companions/#{account.name}/#{companion.name}", headers: auth_headers(user)
+          expect(response).to have_http_status(200)
+          expect(response.parsed_body['name']).to eq(companion.name)
+        end
+      end
+
+      context 'when the requesting user is not the account owner' do
+        let!(:other_user) { create(:user) }
+        it 'returns a 404' do
+          get "/companions/#{account.name}/#{companion.name}", headers: auth_headers(other_user)
+          expect(response).to have_http_status(404)
+        end
       end
     end
   end
